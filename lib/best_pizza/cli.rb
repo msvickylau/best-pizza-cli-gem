f3362579f0a9de56072e536a0312ce6e2740c995
Vicky Lau <vlau.nyc@gmail.com>
2017-10-11 21:57:40 -0400

    revise menu, revise find

--- lib/best_pizza/cli.rb
+++ lib/best_pizza/cli.rb
@@ -1,13 +1,14 @@
 class BestPizza::CLI
 
 	def call
-		BestPizza::Resturant.pizza_restaurants
+		BestPizza::Resturant.scrape_pizza
 		list_pizza
 		menu
 		repeat
 	end
 
 	def list_pizza
+		puts ""
 		puts "====================== Best New York Pizza ======================"
 		puts ""
 		@pizzas = BestPizza::Resturant.pizza_restaurants
@@ -18,34 +19,38 @@ class BestPizza::CLI
 
 	def menu
 		input = nil
+
 		while input != "exit"
 			puts ""
 			puts "Which delicious pizza restaurant would you like more information on?"
+			
 			input = gets.strip.downcase
+			# if input.to_i.between?(1,11)
 
-			if input.to_i.between?(1,87)
-				@resturant = BestPizza::Resturant.find(input.to_i)
-				resturant_card
+			if input.to_i > 0 && input.to_i <= BestPizza::Resturant.pizza_restaurants.size
+				restaurant = BestPizza::Resturant.find(input)
+				# restaurant_card(restaurant)
+
+				puts "================= #{pizza.name} ================="
+				puts ""
+				puts " #{pizza.description}"
+				puts ""
+				puts "------------------------------------------------------"
+				puts ""
+				puts " "
+				puts " "
+				puts ""
+
+				repeat
+
+			elsif input == "list"
+				list_pizza
 			elsif input == "exit"
 				thanks
 			else
-				puts "Not sure what you're looking for, type 1-87 or 'exit'"
+				puts "Not sure what you're looking for, type 1-11, 'list', or 'exit'"
 			end
 		end		
-	end
-
-	def resturant_card
-		puts "================= #{pizza.name} ================="
-		puts ""
-		puts " #{pizza.description}"
-		puts ""
-		puts "------------------------------------------------------"
-		puts ""
-		puts " "
-		puts "  AREA: 					#{pizza.area}"
-		puts ""
-
-		repeat
 	end
 
 	def repeat
--- lib/best_pizza/restaurant.rb
+++ lib/best_pizza/restaurant.rb
@@ -2,15 +2,18 @@ class BestPizza::Resturant
 
 	attr_accessor :name, :description, :area
 
+	@@pizza_restaurants = []
+
+
 	def self.pizza_restaurants
-		self.scrape_pizza
+		@@pizza_restaurants
 	end
 
-	def self.scrape_details
-		pizzas = []
-		pizzas << self.scrape_pizza
-		pizzas
-	end
+	# def self.scrape_details
+	# 	pizzas = []
+	# 	pizzas << self.scrape_pizza
+	# 	pizzas
+	# end
 
 	def self.scrape_pizza
 		doc = Nokogiri::HTML(open("https://www.tripsavvy.com/best-pizza-in-new-york-city-1613413"))
@@ -19,14 +22,15 @@ class BestPizza::Resturant
 			pizza = self.new
 		  pizza.name = info.css('h3.ordered-list__header').text.strip
 		  pizza.description = info.css('div.inline-chop_2-0').text.strip
-		  pizza.area = info.css('div.c-mapstack__address').text.strip
+		  
 			
-			pizza
+			# pizza.area || = info.xpath("//*[@id="inline-chop_2-0-2"]/ul/li[1]").text.strip
+			self.pizza_restaurants << pizza
 		end
 	end
 
 	 def self.find(id)
-    self.scrape_pizza[id.to_i - 1]
+    self.pizza_restaurants[id.to_i - 1]
   end
 end
 
